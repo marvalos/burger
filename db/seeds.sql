dd476cff1c8842f51df4f222b4a059542c4f5764
Marc Avalos <mavalos@email.arizona.edu>
2019-11-02 14:15:04 -0700

    app, db, config, model, controller, view set up

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Green Chile Double');
+INSERT INTO burgers (burger_name) VALUES ('Western Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Triple Double');
